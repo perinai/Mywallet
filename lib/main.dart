a2da0dc3b7c0f27babffa7af70059a6b91762722
Slozzyondul <solomonondula@gmail.com>
2024-12-09 22:44:57 +0300

    merging class updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:my_pocket_wallet/screens/loginscreen.dart';
 import 'package:my_pocket_wallet/screens/splashscreen.dart'; // Importing the Material package.
 
 void main() {
@@ -18,10 +17,7 @@ class MyPocketWallet extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'My Pocket Wallet',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home:  Splashscreen(), // Initial screen.
+      home: const Splashscreen(), // Initial screen.
     );
   }
 }
-
-
-
--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -3,7 +3,7 @@ import 'package:my_pocket_wallet/screens/loginscreen.dart';
 
 
 class Splashscreen extends StatefulWidget {
-  Splashscreen({Key? key}) : super(key: key);
+  const Splashscreen({Key? key}) : super(key: key);
 
   @override
   _SplashscreenState createState() => _SplashscreenState();
@@ -35,7 +35,7 @@ Widget _topImageSection() {
 
 Widget _middleScreenText() {
   return Container(
-    child: Column(
+    child: const Column(
       children: [
         Text("User Sunday"),
         SizedBox(
@@ -54,7 +54,7 @@ Widget _spashButton(context) {
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         FloatingActionButton(
-          shape: RoundedRectangleBorder(
+          shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(
               Radius.circular(30),
             ),
@@ -68,7 +68,7 @@ Widget _spashButton(context) {
             MaterialPageRoute(builder: (context) => const LoginPage()),
           );
           },
-          child: Icon(Icons.arrow_forward),
+          child: const Icon(Icons.arrow_forward),
         ),
       ],
     ),
